22447bc1530c873a7de9368e05c01ca71093d1fa
FelipeGPrazeres <felipegalvaoprazeres@gmail.com>
2024-09-25 21:55:38 -0300

    6 exercícios

--- atividade2.py
+++ atividade2.py
@@ -3,9 +3,9 @@ def divisao(a,b):
         divis = a / b
         return divis
     except ValueError:
-        return "Um dos números não é inteiro"
+        return "Uma das entradas não é um número"
     except ZeroDivisionError:
-        return "A divisão não é possível"
+        return "A divisão por 0 não é possível"
 resultado = divisao(5,0)
 print(resultado)
 resultado =divisao(4,2)
